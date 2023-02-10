6f93f4f2f47b9b5e2bfb03c930890a71c5466023
Dennis <dennis.know.why@gmail.com>
2023-02-10 23:52:00 +0800

    建立seed factory

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Todo;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class TodoFactory extends Factory
+{
+    protected $model = Todo::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->name,
+            'content' => $this->faker->text,
+            'finished' => $this->faker->boolean,
+            'todo_time' => $this->faker->dateTimeThisYear(),
+            'user_id' => User::all()->random()->id
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Todo;
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        //關閉外鍵約束
+        Schema::disableForeignKeyConstraints();
+
+        Todo::truncate();
+        User::truncate();
+
+        User::factory(5)->create();
+        Todo::factory(10000)->create();
+
+        //啟動外鍵約束
+        Schema::enableForeignKeyConstraints();
     }
 }
